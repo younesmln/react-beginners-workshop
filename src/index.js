0da8ace916062edc464b189e23d9b531c05fc765
Younes meliani <dev.meliani@gmail.com>
2018-08-27 09:13:31 +0200

    second week code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SearchBox from "./SearchBox";
-import List from "./List";
+import Clock from "./components/Clock";
+import "./style.css";
 
 const root = document.getElementById("root");
 
-function myFunc(arg) {
-  console.log(arg);
-}
-
 function App() {
   return (
-    <div id="test">
-      {/* <SearchBox
-        label="search"
-        inputPlaceholder="search for something"
-        onInputChange={myFunc}
-      />
-      <SearchBox label="search for" />
-      <SearchBox />
-      <SearchBox inputPlaceholder="test" /> */}
-      <List items={["text1", "text2"]} />
-      <List items={["todo 2", "todo 1", "dinner"]} />
+    <div id="app">
+      <Clock completed={false} />
     </div>
   );
 }
